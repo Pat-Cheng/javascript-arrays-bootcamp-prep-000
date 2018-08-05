e94d5dd98b53c0214cd13fb1d3b7ef3ace950379
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-08-05 07:14:28 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var chocolateBars = ['snickers', 'hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(a,b){
+  return [b,...a]
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+  a.unshift(b)
+  return a
+}
+
+function addElementToEndOfArray(a,b){
+  return [...a,b]
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  a.push(b)
+  return a
+}
+
+function accessElementInArray(a,b){
+  return a[b]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+  
+}
